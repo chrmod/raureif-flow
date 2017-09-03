a063752ccc55021d6d8e72668cf0be335ae22765
Krzysztof Jan Modras <chrmod@chrmod.net>
2017-09-03 19:37:57 +0200

    Configure linter and print flow errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import flowPreset from 'babel-preset-flow';
 import eslintOptions from 'eslint-config-airbnb-flow/eslintrc.json';
+import FlowFilter from 'broccoli-flow';
+import flowtypePlugin from 'eslint-plugin-flowtype';
 
 export default {
   babelOptions: {
@@ -8,5 +10,16 @@ export default {
       flowPreset,
     ],
   },
-  eslintOptions,
+  eslintOptions: Object.assign({}, eslintOptions, {
+    // TODO: find a better way to set parser
+    parser: require.resolve('babel-eslint'),
+  }),
+  eslintPlugins: {
+    flowtype: flowtypePlugin,
+  },
+  build(inputTree) {
+    return new FlowFilter(inputTree, {
+      printErrors: true,
+    });
+  },
 };
